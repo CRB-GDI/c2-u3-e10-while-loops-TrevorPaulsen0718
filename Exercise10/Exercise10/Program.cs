e5de213ad1d2ba366808ec7d60646b1c4df1c6bf
TrevorPaulsen0718 <trevor.paulsen@s.capregboces.org>
2024-03-14 08:20:54 -0400

    Changes

---
+++ Exercise10/Exercise10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+
+namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("**************");
+
+
+
+        }
+    }
+}
