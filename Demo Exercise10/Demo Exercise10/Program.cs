370c3c4073081df802b8f81ddc050fed429c9545
TrevorPaulsen0718 <trevor.paulsen@s.capregboces.org>
2024-03-14 09:42:37 -0400

    changes

---
+++ Demo Exercise10/Demo Exercise10/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Demo_Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int enemies = 18;
+            int playerHP = 10;
+
+
+            while (enemies > 0 && playerHP > 5)
+                {
+                //Attack Enemies
+                enemies--;
+                Console.WriteLine("Enemies left: " + enemies);
+                // Random damage to player may reduce HP
+
+                } // End of while loop
+
+                if (enemies <= 0);
+                {
+                Console.WriteLine("Congratulations, you win!");
+                }
+                else
+                {
+                Console.WriteLine("You Lose Game Over");
+                }
+                }
+
+            }
+        }
+    }
+}
--- Exercise10/Exercise10/Program.cs
+++ Exercise10/Exercise10/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.Design;
+using System.Xml.Serialization;
 
 namespace Exercise10
 {
     internal class Program
     {
         static void Main(string[] args)
+          
         {
+            int choice;
             Console.WriteLine("**************");
             Console.WriteLine("Menu: ");
             Console.WriteLine("1 - New Game");
@@ -13,8 +16,19 @@ namespace Exercise10
             Console.WriteLine("3 - Options");
             Console.WriteLine("4 - Quit");
             Console.WriteLine("**************");
+            Console.WriteLine("");
 
+            // Choice
+            Console.Write("Select Choice (1, 2, 3, 4): ");
+            choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
 
+            while (choice < 4)
+            {
+                if (choice == 1);
+                Console.WriteLine("Starting New Game");
+
+            }
 
         }
     }
